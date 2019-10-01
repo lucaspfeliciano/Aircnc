fdc5cab4cc955059c84ea5de6ce4f87af4c71205
Lucas Feliciano <lucas.feliciano@bry.com.br>
2019-10-01 17:24:35 -0300

    Primeira aula

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const routes = require('./routes');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://lucasfeliciano:123@cluster0-uax4l.mongodb.net/semana09?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
